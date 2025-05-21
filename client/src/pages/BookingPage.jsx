e5fd92320703f46a6e0fa2d45d46534ce009d7b4
nicoalvarez03 <nicooalvarez03@gmail.com>
2025-05-21 20:29:56 +0200

    feat: creacion endpoint deleteBooking e implementacion de borrar una reserva

--- client/src/pages/BookingPage.jsx
+++ client/src/pages/BookingPage.jsx
@@ -1,13 +1,18 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useParams } from "react-router-dom";
 import AddressLink from "../AddressLink";
 import PlaceGallery from "../PlaceGallery";
 import BookingDates from "../BookingDates";
+import DeleteModal from "../DeleteModal";
+import toast from "react-hot-toast";
 
 export default function BookingPage() {
     const {id} = useParams();
     const [booking, setBooking] = useState(null);
+    const [redirect, setRedirect] = useState(null);
+    const [modalOpen, setModalOpen] = useState(false);
+
     useEffect(() => {
         if(id){
             axios.get('/bookings').then(response => {
@@ -19,14 +24,41 @@ export default function BookingPage() {
         }
     }, [id]);
 
+    async function deleteBooking() {
+        await axios.delete('/bookings/'+id);
+        toast.success('Reserva eliminada con éxito');
+        setModalOpen(false);
+        setRedirect('/account/bookings');
+    }
+
+    if(redirect){
+        return <Navigate to={redirect} />
+    }
+
     if(!booking){
         return '';
     }
 
+
+
     return (
         <div className="my-8 px-5">
-            <h1 className="text-2xl font-semibold md:text-3xl">{booking.place.title}</h1>
-            <AddressLink className="my-2 block">{booking.place.address}</AddressLink>
+            <div className="flex flex-col md:flex-row justify-between items-center">
+                <div>
+                    <h1 className="text-2xl font-semibold md:text-3xl">{booking.place.title}</h1>
+                    <AddressLink className="my-2 block">{booking.place.address}</AddressLink>
+                </div>
+                <div className="mr-2">
+                    <button 
+                        className="button-primary w-40 md:w-50 cursor-pointer hover:bg-[#ff5f92] hover:scale-110 transition-all h-12"
+                        onClick={() => setModalOpen(true)}
+                    >
+                        <span className="text-sm md:text-[16px]">Eliminar reserva</span>
+                    </button>
+                </div>
+            </div>
+            
+            
             <div className="bg-gray-200 p-2 md:p-6 my-6 rounded-2xl flex items-center justify-between">
                 <div>
                     <h2 className="text-xl md:text-2xl mb-4">Información sobre tu reserva</h2>
@@ -38,6 +70,14 @@ export default function BookingPage() {
                 </div>               
             </div>
             <PlaceGallery place={booking.place} />
+
+            {/* Modal de confimar eliminación */}
+            <DeleteModal 
+                isOpen={modalOpen}
+                onClose={() => setModalOpen(false)}
+                onDelete={deleteBooking}
+                title="¿Estás seguro que quieres eliminar esta reserva?"
+            />
         </div>
     );
 }
